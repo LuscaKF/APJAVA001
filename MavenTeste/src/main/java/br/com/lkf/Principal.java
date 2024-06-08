e814e5c0fb4919b09eda47078f7089b5a36b3bef
Ana Paula Kirow Juca <ana.paula@plural.local>
2024-06-08 16:37:48 -0300

    Iniciando com o framework Maven

---
+++ MavenTeste/src/main/java/br/com/lkf/Principal.java
@@ -0,0 +1,11 @@
+package br.com.lkf;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bem-vindo Maven!");
+
+	}
+
+}
