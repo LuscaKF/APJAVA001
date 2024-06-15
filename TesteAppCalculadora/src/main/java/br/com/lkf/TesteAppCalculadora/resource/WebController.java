044416eba7c50602698e252f010aa80bb5148cdf
Ana Paula Kirow Juca <ana.paula@plural.local>
2024-06-15 09:14:19 -0300

    Iniciando com o Spring Framework e Spring boot

---
+++ TesteAppCalculadora/src/main/java/br/com/lkf/TesteAppCalculadora/resource/WebController.java
@@ -0,0 +1,13 @@
+package br.com.lkf.TesteAppCalculadora.resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+	@GetMapping("/")
+	public String showForm(Model model) {
+		return "index";
+	}
+}
