786ecdbb0e3abd87eb724e2114753ce0ae521678
Ana Paula Kirow Juca <ana.paula@plural.local>
2024-06-15 10:03:07 -0300

    Criação de uma calculadora simples

--- TesteAppCalculadora/src/main/java/br/com/lkf/TesteAppCalculadora/resource/WebController.java
+++ TesteAppCalculadora/src/main/java/br/com/lkf/TesteAppCalculadora/resource/WebController.java
@@ -1,13 +1,32 @@
 package br.com.lkf.TesteAppCalculadora.resource;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 	@GetMapping("/")
 	public String showForm(Model model) {
+		model.addAttribute("modelOperacoes", List.of("Soma", "Subtração", "Mulltiplicação", "Divisão"));
+		return "index";
+	}
+	
+	@PostMapping
+	public String handleFormSubmission(
+			@RequestParam String modelOperacoes, 
+			@RequestParam String valor01, 
+			@RequestParam String valor02, 
+		    Model model) {
+		
+		System.out.println("modelOperacoes: " + modelOperacoes + ", " + 
+						   "valor01: " + valor01 + ", " + 
+						   "valor02: " + valor02);
+		
 		return "index";
 	}
 }
