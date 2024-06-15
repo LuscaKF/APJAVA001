417489c76d757967cdf98199d333cd03dec6bc4c
Ana Paula Kirow Juca <ana.paula@plural.local>
2024-06-15 16:36:42 -0300

    Configuração completa da API com o Objeto produto

--- AppProdutos/src/main/java/br/com/lkf/AppProdutos/resource/ProdutoResource.java
+++ AppProdutos/src/main/java/br/com/lkf/AppProdutos/resource/ProdutoResource.java
@@ -1,8 +1,17 @@
 package br.com.lkf.AppProdutos.resource;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +24,7 @@ public class ProdutoResource {
 	@Autowired
 	ProdutoService produtoService;
 	
+	/*
 	@GetMapping("produto") // http://localhost:8081/api/produtos/produto
 	public ResponseEntity<Produto> getProduto() {
 		Produto produto = new Produto();
@@ -26,7 +36,7 @@ public class ProdutoResource {
 	}
 	
 	@GetMapping("salvar") // http://localhost:8081/api/produtos/salvar
-	public ResponseEntity<Produto> save() {
+	public ResponseEntity<Produto> save2() {
 		Produto produto = new Produto();
 		produto.setCodigoBarras("123456");
 		produto.setNome("Produto 1");
@@ -38,5 +48,53 @@ public class ProdutoResource {
 			ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(produtoResposta);
+	}
+	
+	*/
+	
+	@GetMapping // http://localhost:8081/api/produtos
+	public ResponseEntity<List<Produto>> findAllProdutos() {
+		List<Produto> produtos = produtoService.findAll();
+		if(produtos == null) {
+			return ResponseEntity.notFound().build();
+		}
+		if(produtos.size() == 0) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(produtos);
+	}
+	
+	@GetMapping("/{id}") // http://localhost:8081/api/produtos/2
+	public ResponseEntity<Optional<Produto>> findById(@PathVariable Long id) {
+		Optional<Produto> produto = produtoService.findById(id);
+		if(produto.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(produto);
+	}
+	
+	@PostMapping // http://localhost:8081/api/produtos
+	public ResponseEntity<Produto> save(@RequestBody Produto produto) {
+		Produto newProduto = produtoService.save(produto);
+		if(newProduto == null) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(newProduto);
+		}
+	}
+	
+	@PutMapping // http://localhost:8081/api/produtos
+	public ResponseEntity<Produto> update(@RequestBody Produto produto) {
+		Produto updProduto = produtoService.update(produto);
+		if(updProduto == null) 
+			return ResponseEntity.notFound().build(); 
+		return ResponseEntity.ok(updProduto);
+		
+	}
+	
+	@DeleteMapping("/{id}") // http://localhost:8081/api/produtos/2
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		produtoService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT); // Status code 204
 	}
 }
--- AppProdutos/src/main/java/br/com/lkf/AppProdutos/service/ProdutoService.java
+++ AppProdutos/src/main/java/br/com/lkf/AppProdutos/service/ProdutoService.java
@@ -1,5 +1,8 @@
 package br.com.lkf.AppProdutos.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +25,42 @@ public class ProdutoService {
 		}
 		
 		// Gravar
-		return produtoRepository.save(produto);
+		try {
+			return produtoRepository.save(produto); // Para o banco de dados
+		} catch (Exception e) {
+			System.out.println("ERR: Erro ao inserir produto " + produto.toString() + ":" + e.getMessage());
+			return null;
+		}
+		
+	}
+	
+	public List<Produto> findAll() {
+		return produtoRepository.findAll();
+	}
+	
+	public Optional<Produto> findById(Long id) {
+		return produtoRepository.findById(id);
+	}
+	
+	public Produto update(Produto produto) {
+		// Pesquisar se o produto existe
+		Optional<Produto> findProduto = produtoRepository.findById(produto.getId());
+		
+		// Se o produto existir, atualizar
+		if(findProduto.isPresent()) {
+			// Criar um novo objeto do produto e lançar os dados do objeto de parâmetro
+			Produto updProduto = findProduto.get();
+			updProduto.setCodigoBarras(produto.getCodigoBarras());
+			updProduto.setNome(produto.getNome());
+			updProduto.setPreco(produto.getPreco());
+			// Retornar o objeto gravado
+			return produtoRepository.save(updProduto); // "UPDATE"
+		}
+		// Retornar o objeto gravado
+		return produtoRepository.save(produto); // "INSERT"
+	}
+	
+	public void delete(Long id) {
+		produtoRepository.deleteById(id);
 	}
 }
